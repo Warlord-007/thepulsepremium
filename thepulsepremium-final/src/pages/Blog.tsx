d263c9902d98b5992631346de79f4d2338910295
Warlord-007 <k95020487@gmail.com>
2026-03-17 19:35:23 +0000

    Create Blog.tsx

---
+++ thepulsepremium-final/src/pages/Blog.tsx
@@ -0,0 +1,10 @@
+export default function Blog() {
+  return (
+    <div className="min-h-screen pt-24 pb-20 px-6">
+      <div className="max-w-7xl mx-auto text-center">
+        <h1 className="text-4xl font-bold text-white">Blog Page</h1>
+        <p className="text-gray-400 mt-4">If you see this, the build works.</p>
+      </div>
+    </div>
+  )
+}
